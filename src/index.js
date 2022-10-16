4ad6ebefb19787ca9a344269a729e245f7bfb0a3
Hyacinthe N'Goma <hyacinthengoma@gmail.com>
2022-10-16 02:39:16 +0200

    create folder components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+//import React from 'react';
+// import { StrictMode } from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //<React.StrictMode>
     <App />
+ // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
